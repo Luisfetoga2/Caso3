e9af4e507c3e00a7cb43cfc2f146645e3427f199
Luisfetoga2 <l.torresg@uniandes.edu.co>
2023-11-08 19:38:10 -0500

    optimizacion⁴

--- Caso3/src/MineTask.java
+++ Caso3/src/MineTask.java
@@ -26,7 +26,7 @@ public class MineTask implements Runnable {
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-
+        
         if (firstHalf) {
             this.start = "a";
             this.i = 0;
@@ -60,11 +60,11 @@ public class MineTask implements Runnable {
             */
             
             input = data + v;
-            String hash = calculateHash(input);
-            if (startsWithZeros(hash, zeros)) {
+            
+            if (calculateVerifyHash(input, zeros)) {
                 long endTime = System.currentTimeMillis();
                 // print result
-                monitor.printResult(id, startTime, endTime, hash, data, v, zeros);
+                monitor.printResult(id, startTime, endTime, calculateHash(input), data, v, zeros);
                 return;
             } 
             else if (v.equals(end)) {
@@ -80,11 +80,16 @@ public class MineTask implements Runnable {
     private String nextString(String v) {
         
         // Incrementa el último carácter o cambia 'z' por 'a' y propaga si es necesario
-        while (charArray[i] == 'z') {
-            charArray[i] = 'a';
-            i--;
-            if (i == -1) {
-                break;
+        boolean sentinel = true;
+        while (sentinel) {
+            if (charArray[i] == 'z') {
+                charArray[i] = 'a';
+                i--;
+                if (i == -1) {
+                    sentinel = false;
+                }
+            } else {
+                sentinel = false;
             }
         }
         // Si todos los caracteres eran 'z', agrega un 'a' al principio
@@ -112,14 +117,47 @@ public class MineTask implements Runnable {
         return sb.toString();
     }
 
-    private boolean startsWithZeros(String hash, int zeros) {
+    private boolean calculateVerifyHash(String input, int zeros) {
+        byte[] hashBytes = md.digest(input.getBytes());
+        
+        int count = 0;
+        //System.out.println("-");
+        for (byte b : hashBytes) {
+            String a = String.format("%02x", b);
+            //System.out.println("a: " + a);
+            //System.out.println("count: "+ count);
+            if (a.equals("00")) {
+                count+=2;
+                if (count>=zeros) {
+                    return true;
+                }
+            }
+            else if (a.charAt(0)=='0') {
+                count+=1;
+                if (count!=zeros){
+                    return false;
+                } else {
+                    return true;
+                }
+            } else {
+                if (count<zeros) { 
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+        return false; 
+    }
+
+    /*private boolean startsWithZeros(String hash, int zeros) {
         for (int x = 0; x < zeros; x++) {
             if (hash.charAt(x) != '0') {
                 return false;
             }
         }
         return true;
-    }
+    }*/
 }
 
 
